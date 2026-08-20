e80c6d637a04eb15ece18b4307bdd226eb343150
TheGolden9321 <shampunzhumaysynbaskazhy@gmail.com>
2026-08-20 23:27:47 +0300

    fix: move use client directive to top of login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-dynamic';
 'use client'
+
+
 import { useState, useEffect, useRef } from 'react'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
